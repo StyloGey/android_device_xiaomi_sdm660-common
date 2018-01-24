a98eb77f1bbe6bd02380e36c68afeb722cc58823
Offain <fabian.leutenegger@bluewin.ch>
2018-01-24 20:52:29 +0100

    sdm660-common: Drop local seccomp
     * We already have it in our platform
    
    Change-Id: Ia9547d8c547a49c3cbd1b7f6a7f0a6b2076854f7

--- sdm660.mk
+++ sdm660.mk
@@ -434,11 +434,6 @@ PRODUCT_BOOT_JARS += \
     ims-ext-common_system \
     telephony-ext
 
-# Seccomp policy
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
-    $(COMMON_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
