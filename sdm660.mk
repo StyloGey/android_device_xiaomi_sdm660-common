01ce3a7468720894f4e76527ffa1e9b28930f834
Stylogey <stylogey@gmail.com>
2019-12-25 02:30:14 +0100

    sdm660-common: Add doubletap gesture for PA

--- sdm660.mk
+++ sdm660.mk
@@ -298,7 +298,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(COMMON_PATH)/overlay
+    $(COMMON_PATH)/overlay \
+    $(COMMON_PATH)/overlay-pa
 
 # Permissions
 PRODUCT_COPY_FILES += \
