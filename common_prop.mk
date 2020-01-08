7e8261a2bc23d5587bce03560278225e54545753
Stylogey <stylogey@gmail.com>
2020-01-08 22:23:53 +0100

    sdm660-common: Switch to ParanoidDoze

--- common_prop.mk
+++ common_prop.mk
@@ -155,6 +155,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.high_fps_early_gl_phase_offset_ns=9000000 \
     debug.sf.phase_offset_threshold_for_next_vsync_ns=6100000
 
+# Doze
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sensor.proximity=true \
+    ro.sensor.pickup=android.sensor.tilt_detector
+
 # FIFO scheduling for UI
 PRODUCT_PROPERTY_OVERRIDES += \
     sys.use_fifo_ui=1
--- sdm660.mk
+++ sdm660.mk
@@ -156,7 +156,7 @@ PRODUCT_PACKAGES += \
 
 # Doze
 PRODUCT_PACKAGES += \
-    XiaomiDoze
+    ParanoidDoze
 
 # DRM
 PRODUCT_PACKAGES += \
